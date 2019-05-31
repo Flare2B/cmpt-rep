b16f7a29e88738ae2ff190e73fa557f5d2c6faf4
LeoC <leonardo.chiarotti@gmail.com>
2019-05-31 09:26:34 +0200

    Added a useless feature that nobody asked for

--- main.cpp
+++ main.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 void main()
 {
-	for(int i = 0; i < 10; i++) /*Nice comment*/
+	for(int i = 0; i < 10; i++)
 	{
 		cout << "Valore " << i + 2;
+		cout << "+++";
 	}
 	system("PAUSE");
 }
