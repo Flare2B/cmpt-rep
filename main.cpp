5225dd117e2aee9211f3b13e5b916aefe9b849ba
LeoC <leonardo.chiarotti@gmail.com>
2019-05-31 09:22:57 +0200

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+	for(int i = 0; i < 10; i++)
+	{
+<<<<<<< HEAD
+		cout << "Valore " << i + 2;
+=======
+		cout << "Valore " << i + 4;
+>>>>>>> FIX_BUG
+	}
+	system("PAUSE");
+}
